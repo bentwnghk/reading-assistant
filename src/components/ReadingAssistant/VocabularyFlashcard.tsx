d2bab74996013abacee46463977f75b9a45e8f1f
Ben Ng <mr.ng@mystudies.net>
2026-03-15 00:58:07 +0800

    Update VocabularyFlashcard.tsx

--- src/components/ReadingAssistant/VocabularyFlashcard.tsx
+++ src/components/ReadingAssistant/VocabularyFlashcard.tsx
@@ -355,7 +355,7 @@ function VocabularyFlashcard({ glossary }: VocabularyFlashcardProps) {
             <div className="flex items-center justify-center gap-2 mb-4">
               <div className={cn(
                 "font-extrabold text-center",
-                (currentEntry.syllabification || currentEntry.word).length > 17 ? "text-3xl" : (currentEntry.syllabification || currentEntry.word).length > 11 ? "text-4xl" : "text-5xl"
+                (currentEntry.syllabification || currentEntry.word).length > 18 ? "text-3xl" : (currentEntry.syllabification || currentEntry.word).length > 12 ? "text-4xl" : "text-5xl"
               )}>
                 {currentEntry.syllabification || currentEntry.word}
               </div>
