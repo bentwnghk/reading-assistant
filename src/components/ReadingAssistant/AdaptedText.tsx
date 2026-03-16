ea6e20eaf6d9177e1180cc0d3bcc82fff94772c6
Ben Ng <mr.ng@mystudies.net>
2026-03-16 21:16:07 +0800

    v2.85

--- src/components/ReadingAssistant/AdaptedText.tsx
+++ src/components/ReadingAssistant/AdaptedText.tsx
@@ -984,6 +984,7 @@ function AdaptedText() {
                   <div>
                     <h5 className="font-medium text-foreground">{t("reading.adaptedText.help.features.title")}</h5>
                     <ul className="list-disc list-inside text-muted-foreground space-y-1 mt-1">
+                      <li>{t("reading.adaptedText.help.features.edit")}</li>
                       <li>{t("reading.adaptedText.help.features.highlight")}</li>
                       <li>{t("reading.adaptedText.help.features.tts")}</li>
                       <li>{t("reading.adaptedText.help.features.sentenceAnalysis")}</li>
