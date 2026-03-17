5bcc1f817f56cb0217d151298c5e545cdda7daed
Ben Ng <mr.ng@mystudies.net>
2026-03-17 19:19:54 +0800

    v2.94

--- src/components/ReadingAssistant/AdaptedText.tsx
+++ src/components/ReadingAssistant/AdaptedText.tsx
@@ -1418,8 +1418,8 @@ function AdaptedText() {
         open={!!activeSentence}
         onOpenChange={(open) => !open && setActiveSentence(null)}
       >
-        <DialogContent className="max-w-[calc(100vw-2rem)] mx-2 max-h-[80vh] overflow-y-auto p-4 sm:p-6 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
-          <DialogHeader>
+        <DialogContent className="w-[calc(100vw-1.5rem)] max-w-md max-h-[80vh] overflow-x-hidden overflow-y-auto p-3 sm:p-6 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+          <DialogHeader className="pr-6">
             <DialogTitle>
               {t("reading.extractedText.analysisTitle")}
             </DialogTitle>
@@ -1427,7 +1427,7 @@ function AdaptedText() {
               {t("reading.extractedText.analysisDescription")}
             </DialogDescription>
           </DialogHeader>
-          <div className="prose prose-sm dark:prose-invert max-w-full pr-2">
+          <div className="prose prose-sm dark:prose-invert w-full max-w-none">
             {isAnalysisLoading && !activeAnalysis ? (
               <div className="flex items-center justify-center py-8">
                 <Loader2 className="h-6 w-6 animate-spin mr-2" />
