c0ea05c74f55eef00d72730c6e7fc4eb8f571a97
Ben Ng <mr.ng@mystudies.net>
2026-04-09 10:28:19 +0800

    v2.379

--- src/components/ReadingAssistant/AdaptedText.tsx
+++ src/components/ReadingAssistant/AdaptedText.tsx
@@ -965,7 +965,7 @@ function AdaptedText() {
 
   return (
     <section className="p-4 border rounded-md mt-4">
-      <div className="flex items-center justify-between border-b pb-4 mb-4">
+      <div className="flex flex-wrap md:flex-nowrap items-center justify-between border-b pb-4 mb-4 gap-2">
         <h3 className="font-semibold text-lg flex items-center gap-2">
           <BookOpen className="h-5 w-5 text-muted-foreground" />
           {t("reading.adaptedText.title")}
