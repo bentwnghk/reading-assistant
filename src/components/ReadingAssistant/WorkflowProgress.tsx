cb631f4ce1414fd65a8e07b05cf3bf00f99d7242
Ben Ng <mr.ng@mystudies.net>
2026-05-09 06:55:29 +0800

    v2.497

--- src/components/ReadingAssistant/WorkflowProgress.tsx
+++ src/components/ReadingAssistant/WorkflowProgress.tsx
@@ -75,7 +75,7 @@ function WorkflowProgress() {
         />
       </div>
 
-      <div className="flex flex-wrap justify-center gap-x-2 gap-y-2 sm:justify-between">
+      <div className="flex flex-wrap justify-between gap-x-2 gap-y-2">
         {steps.map((step, index) => (
           <div
             key={step.key}
