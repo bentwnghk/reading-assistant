5182ec047149d5502a0e92352930915148fd7027
Ben Ng <mr.ng@mystudies.net>
2026-08-10 22:40:16 +0800

    v3.70

--- src/components/ReadingAssistant/TextRepository.tsx
+++ src/components/ReadingAssistant/TextRepository.tsx
@@ -898,7 +898,7 @@ function TextRepository({ onTextLoaded }: TextRepositoryProps) {
           <Table>
             <TableHeader className="sticky top-0 z-10 bg-background">
               <TableRow>
-                <TableHead className="min-w-[200px]">
+                <TableHead className="min-w-[220px]">
                   <SortButton
                     field="name"
                     current={sortField}
