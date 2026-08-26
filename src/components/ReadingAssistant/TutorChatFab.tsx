c2092cbef4aa671bd138dc5c02a1d1db9223a1e8
Ben Ng <mr.ng@mystudies.net>
2026-08-26 14:51:32 +0800

    v4.29 (Move TutorChatFab downwards)

--- src/components/ReadingAssistant/TutorChatFab.tsx
+++ src/components/ReadingAssistant/TutorChatFab.tsx
@@ -44,7 +44,7 @@ function TutorChatFab() {
         onClick={handleToggle}
         onTouchEnd={(e) => { e.preventDefault(); handleToggle(); }}
         className={cn(
-          "fixed bottom-20 right-6 z-50 h-12 w-12 md:w-auto md:px-5 rounded-full shadow-lg print:hidden",
+          "fixed bottom-6 right-6 z-50 h-12 w-12 md:w-auto md:px-5 rounded-full shadow-lg print:hidden",
           "bg-gradient-to-br from-primary to-primary/80 hover:from-primary/90 hover:to-primary/70",
           "transition-all duration-300",
           openTutorChat && "scale-0 opacity-0"
